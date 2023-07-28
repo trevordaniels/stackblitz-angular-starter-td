22729a5380c410ff433c2764e39accac5f3341f7
trevordaniels <trevor.daniels@gmail.com>
2023-07-28 13:12:20 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-angular-starter-td
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-nh2e2b)
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    Simple angular app
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
